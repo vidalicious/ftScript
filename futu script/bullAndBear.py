c4be2ae7356eabda95891fb6f8343a911211ea2b
wdgu <wdgu@Ctrip.com>
2016-04-22 14:21:30 +0800

    hello~ bull and bear

---
+++ futu script/bullAndBear.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import time
+from FSApi import *
+from math import *
+
+
---
+++ futu script/bullAndBearSampling.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import time
+from FSApi import *
+from math import *
+
+host = "localhost"
+port = 11111
+
+targetCode = "800000" # 恒指
+
+bullCode = "65205"
+bearCode = "64940"
+
+oneTickTime = 1
+counter = 0
+
+connectSocket = connect(host, port)
+if connectSocket is not None:
+    while True:
+        file = open("bull and bear data", "a+")
+
+        targetPrice = getCurrentPrice(connectSocket, targetCode)
+        print "target ", targetCode, " currentPrice ", floatPrice(targetPrice), " count ", counter, " time ", time.strftime('%Y-%m-%d %H:%M:%S')
+        log = ["target ", targetCode, " currentPrice ", str(floatPrice(targetPrice)), " count ", str(counter), " time ", time.strftime('%Y-%m-%d %H:%M:%S'), "\n"]
+        file.writelines(log)
+
+        bullPrice = getCurrentPrice(connectSocket, bullCode)
+        print "bull ", bullCode, " current price ", floatPrice(bullPrice)
+        log = ["bull ", bullCode, " current price ", floatPrice(bullPrice), "\n"]
+        file.writelines(log)
+
+        bearPrice = getCurrentPrice(connectSocket, bearCode)
+        print "bear ", bearCode, " current price ", floatPrice(bearPrice)
+        log = ["bear ", bearCode, " current price ", floatPrice(bearPrice), "\n"]
+        file.writelines(log)
+
+        log = ["==========================================================================================\n"]
+        file.writelines(log)
+
+        file.close()
+        counter += 1
+        time.sleep(oneTickTime)
+    disconnect(connectSocket)
