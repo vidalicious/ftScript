0d9a74159c2732b78e6e26752916b908f997aa20
vidalicious <vidalicious@sina.com>
2016-04-24 02:49:24 +0800

    pathTag

--- futu script/bull.py
+++ futu script/bull.py
@@ -53,6 +53,7 @@ bullGear_sellSignal = False
 bullBuySignal = False
 bullSellSignal = False
 
+pathTag = []
 
 # =================================================================
 connectSocket = connect(host, port)
@@ -87,15 +88,20 @@ if connectSocket is not None:
             if meanList[0] > meanList[-1]: #上升趋势
                 if lastRelation == MARelation.BELOW and currentRelation == MARelation.ABOVE:
                     bullTrend_buySignal = True
+                    pathTag.append(" 1 ")
                 if floatPrice(currentTarget) > mean + standardDeviation * bollingerRadius:
                     bullTrend_sellSignal = True
+                    pathTag.append(" 2 ")
             elif meanList[0] < meanList[-1]: #下降趋势
                 if lastRelation == MARelation.ABOVE and currentRelation == MARelation.BELOW:
                     bullTrend_sellSignal = True
+                    pathTag.append(" 3 ")
                 if floatPrice(currentTarget) < mean - standardDeviation * bollingerRadius:
                     bullTrend_buySignal = True
+                    pathTag.append(" 4 ")
             else: #不动趋势
                 bullTrend_sellSignal = True
+                pathTag.append(" 5 ")
 
             bullGearArr = getGearData(connectSocket, bullCode, 1)
             if bullGearArr is not None:
@@ -106,18 +112,23 @@ if connectSocket is not None:
 
                 if bullBuy1Vol / bullSell1Vol > 15:
                     bullGear_buySignal = True
+                    pathTag.append(" 6 ")
                 elif bullSell1Vol / bullBuy1Vol > 15:
                     bullGear_sellSignal = True
+                    pathTag.append(" 7 ")
 
             hasBullPosition = ifHasPositon(positionArr, tradeOneHand, bullCode)
             bullPositionRatio = getPositionRatio(positionArr, bullCode)
 
             if bullTrend_buySignal and bullGear_buySignal:
                 bullBuySignal = True
+                pathTag.append(" 8 ")
             if bullTrend_sellSignal:
                 bullSellSignal = True
+                pathTag.append(" 9 ")
             if bullPositionRatio < -0.03: #止损
                 bullSellSignal = True
+                pathTag.append(" 10 ")
 
             if bullBuySignal:
                 if not hasBullPosition:
@@ -131,12 +142,15 @@ if connectSocket is not None:
                                     hasBuyOrder = True
                                 else:
                                     simu_setOrderStatus(connectSocket, orderInfo["LocalID"], orderInfo["OrderID"], 0)  # 撤单
+                                    pathTag.append(" 撤单 ")
 
                     if not hasBuyOrder:  # 如果没有未成交合适订单则下单
                         localID = simu_commonBuyOrder(connectSocket, currentBullPrice, tradeOneHand, bullCode)
                         print "buy bull", bullCode, " at ", floatPrice(currentBullPrice), "time", time.strftime('%Y-%m-%d %H:%M:%S'), "localID", localID
                         logger = ["buy bull", bullCode, " at ", floatPrice(currentBullPrice), "time", time.strftime('%Y-%m-%d %H:%M:%S'), "localID", localID, "\n"]
                         file.writelines(logger)
+                        pathTag.append("\n")
+                        file.writelines(pathTag)
 
             elif bullSellSignal:
                 if hasBullPosition:
@@ -150,12 +164,15 @@ if connectSocket is not None:
                                     hasSellOrder = True
                                 else:
                                     simu_setOrderStatus(connectSocket, orderInfo["LocalID"], orderInfo["OrderID"], 0)  # 撤单
+                                    pathTag.append(" 撤单 ")
 
                     if not hasSellOrder:
                         localID = simu_commonSellOrder(connectSocket, currentBullPrice, tradeOneHand, bullCode)
                         print "sell bull ", bullCode, " at ", floatPrice(currentBullPrice), "time", time.strftime('%Y-%m-%d %H:%M:%S'), "localID", localID
                         logger = ["sell bull ", bullCode, " at ", floatPrice(currentBullPrice), "time", time.strftime('%Y-%m-%d %H:%M:%S'), "localID", localID, "\n"]
                         file.writelines(logger)
+                        pathTag.append("\n")
+                        file.writelines(pathTag)
 
         # ========== update ===============
         file.close()
@@ -169,6 +186,7 @@ if connectSocket is not None:
         bullGear_sellSignal = False
         bullBuySignal = False
         bullSellSignal = False
+        pathTag = []
 
         time.sleep(oneTickTime)
     disconnect(connectSocket)
