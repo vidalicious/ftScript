f9b49ab534f65d8a2d7bc950896e7d59b312f443
vidalicious <vidalicious@sina.com>
2016-05-23 02:01:32 +0800

    1

--- futu script/bullV11.py
+++ futu script/bullV11.py
@@ -16,9 +16,8 @@ oneTickTime = 10
 host = "localhost"
 port = 11111
 
-targetCode = "999000" # 恒指
+targetCode = "800000" # 恒指
 bullCode = "63331"
-indexCode = "800000"
 bullRecyclePrice = 19650
 tradeOneHand = 10000
 
@@ -67,9 +66,6 @@ if connectSocket is not None:
         # ========== moving average ================
         currentTarget = getCurrentPrice(connectSocket, targetCode)
         print "counter ", str(counter), " target ", str(floatPrice(currentTarget)), " time ", time.strftime('%Y-%m-%d %H:%M:%S')
-
-        indexPrice = getCurrentPrice(connectSocket, indexCode)
-        print "index price ", str(indexPrice)
 
         if flag: # one tick 触发一次
             flag = False
@@ -124,7 +120,7 @@ if connectSocket is not None:
                     simu_checkOrderAndSellWith(connectSocket, tradePrice, positionQty, bullCode, file, pathTag)
 
             else:
-                if abs(floatPrice(indexPrice) - bullRecyclePrice) < 300:
+                if abs(floatPrice(currentTarget) - bullRecyclePrice) < 300:
                     print "too near recycle price"
                 elif not isInGoldenTime():
                     pathTag.append(" not in golden time ")
